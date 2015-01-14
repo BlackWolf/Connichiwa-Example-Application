748f217e163d4f4494161b68012b7ecb91e06bb8
Mario Schreiner <mario.schreiner@uni-konstanz.de>
2015-01-14 14:34:06 +0100

    Updated Connichiwa & Example Application
    - Updated to the latest Version of Connichiwa
    - Created an entirely new example application that is much more advanced, automatically connects remote devices and shows how to use assets (style.css) on the master and on remote devices

--- ConnichiwaExampleApplication/ConnichiwaResources.bundle/weblib/connichiwa.js
+++ ConnichiwaExampleApplication/ConnichiwaResources.bundle/weblib/connichiwa.js
@@ -284,11 +284,15 @@ function CWDevice(properties)
   this.distance = -1;
   var _identifier = properties.identifier;
   var _launchDate = Date.now() / 1000.0;
+  var _ips = [];
+  var _port = undefined;
   var _name = "unknown";
   var _ppi = CWSystemInfo.DEFAULT_PPI();
   var _isLocal = false; 
 
   if (properties.launchDate) _launchDate = properties.launchDate;
+  if (properties.ips) _ips = properties.ips;
+  if (properties.port) _port = properties.port;
   if (properties.name) _name = properties.name;
   if (properties.ppi && properties.ppi > 0) _ppi = properties.ppi;
   if (properties.isLocal) _isLocal = properties.isLocal;
@@ -304,6 +308,10 @@ function CWDevice(properties)
   this.getIdentifier = function() { return _identifier; };
 
   this.getLaunchDate = function() { return _launchDate; };
+
+  this.getIPs = function() { return _ips; };
+
+  this.getPort = function() { return _port; };
 
   this.getName = function() { return _name; };
 
@@ -336,13 +344,28 @@ function CWDevice(properties)
 // DEVICE COMMUNICATION API
 
 
-CWDevice.prototype.append = function(target, html) {
-  Connichiwa.append(this.getIdentifier(), target, html);
+CWDevice.prototype.insert = function(target, html) {
+  Connichiwa.insert(this.getIdentifier(), target, html);
+};
+
+
+CWDevice.prototype.replace = function(target, html) {
+  Connichiwa.replace(this.getIdentifier(), target, html);
+};
+
+
+CWDevice.prototype.replaceContent = function(target, html) {
+  Connichiwa.replaceContent(this.getIdentifier(), target, html);
 };
 
 
 CWDevice.prototype.loadScript = function(url, callback) {
   Connichiwa.loadScript(this.getIdentifier(), url, callback);
+};
+
+
+CWDevice.prototype.loadCSS = function(url) {
+  Connichiwa.loadCSS(this.getIdentifier(), url);
 };
 
 
@@ -1345,8 +1368,10 @@ var CWWebsocketMessageParser = OOP.createSingleton("Connichiwa", "CWWebsocketMes
   "package parse": function(message) {
     switch (message._name) {
       case "ack"               : this._parseAck(message);               break;
-      case "append"            : this._parseAppend(message);            break;
+      case "_insert"           : this._parseInsert(message);            break;
+      case "_replace"          : this._parseReplace(message);           break;
       case "loadscript"        : this._parseLoadScript(message);        break;
+      case "_loadcss"          : this._parseLoadCSS(message);           break;
       case "wasstitched"       : this._parseWasStitched(message);       break;
       case "wasunstitched"     : this._parseWasUnstitched(message);     break;
       case "gotstitchneighbor" : this._parseGotStitchNeighbor(message); break;
@@ -1359,9 +1384,18 @@ var CWWebsocketMessageParser = OOP.createSingleton("Connichiwa", "CWWebsocketMes
     CWEventManager.trigger("__messageack__id" + message.original._id);
   },
 
-  _parseAppend: function(message) {
-    $(message.targetSelector).append(message.html);
+  _parseInsert: function(message) {
+    $(message.selector).append(message.html);
   },
+
+  _parseReplace: function(message) {
+    if (message.contentOnly === true) {
+      $(message.selector).html(message.html);
+    } else {
+      $(message.selector).replaceWith(message.html);
+    }
+  },
+
 
   _parseLoadScript: function(message) {
     var that = this;
@@ -1371,6 +1405,17 @@ var CWWebsocketMessageParser = OOP.createSingleton("Connichiwa", "CWWebsocketMes
       CWDebug.log(1, "There was an error loading '" + message.url + "': " + t);
     });
   },
+
+
+  _parseLoadCSS: function(message) {
+    var cssEntry = document.createElement("link");
+    cssEntry.setAttribute("rel", "stylesheet");
+    cssEntry.setAttribute("type", "text/css");
+    cssEntry.setAttribute("href", message.url);
+    $("head").append(cssEntry);
+    this.package.Connichiwa._sendAck(message);
+  },
+
 
   _parseWasStitched: function(message) {
     CWEventManager.trigger("wasStitched", message);
@@ -1422,7 +1467,7 @@ var Connichiwa = OOP.createSingleton("Connichiwa", "Connichiwa", {
     if (document.readyState === "complete") {
       callback();
     } else {
-      $(window).load(callback);
+      Connichiwa.on("ready", callback);
     }
   },
 
@@ -1430,8 +1475,9 @@ var Connichiwa = OOP.createSingleton("Connichiwa", "Connichiwa", {
   // DEVICE COMMUNICATION API
 
 
-  "public append": function(identifier, target, html) {
-    //With two args, we handle them as identifier and htmlspot
+  "public insert": function(identifier, target, html) {
+    //With two args, we handle them as identifier and html
+    //target is assumed as the body
     if (html === undefined) {
       html = target;
       target = "body";
@@ -1440,7 +1486,7 @@ var Connichiwa = OOP.createSingleton("Connichiwa", "Connichiwa", {
     //target should be a selector but can also be a DOM or jQuery element
     //If so, we try to get it by its ID on the other side
     if (CWUtil.isObject(target)) {
-      target = $(target).attr("id");
+      target = "#"+$(target).attr("id");
     }
     
     //html can be a DOM or jQuery element - if so, send the outerHTML including 
@@ -1452,11 +1498,74 @@ var Connichiwa = OOP.createSingleton("Connichiwa", "Connichiwa", {
       html = clone[0].outerHTML;
     }
 
+    //identifier can also be a CWDevice
+    if (CWDevice.prototype.isPrototypeOf(identifier)) {
+      identifier = identifier.getIdentifier();
+    }
+
     var message = {
-      targetSelector : target,
-      html           : html
+      selector : target,
+      html     : html
     };
-    this.send(identifier, "append", message);
+    this.send(identifier, "_insert", message);
+  },
+
+  "public replace": function(identifier, target, html) {
+    //With two args, we handle them as identifier and html
+    //target is assumed as the body
+    if (html === undefined) {
+      html = target;
+      target = "body";
+    }
+
+    this._replace(identifier, target, html, false);
+  },
+
+  "public replaceContent": function(identifier, target, html) {
+    //With two args, we handle them as identifier and html
+    //target is assumed as the body
+    if (html === undefined) {
+      html = target;
+      target = "body";
+    }
+
+    this._replace(identifier, target, html, true);
+  },
+
+  "private _replace": function(identifier, target, html, contentOnly) {
+    //With two args, we handle them as identifier and html
+    //target is assumed as the body
+    if (html === undefined) {
+      html = target;
+      target = "body";
+    }
+
+    //target should be a selector but can also be a DOM or jQuery element
+    //If so, we try to get it by its ID on the other side
+    if (CWUtil.isObject(target)) {
+      target = "#"+$(target).attr("id");
+    }
+    
+    //html can be a DOM or jQuery element - if so, send the outerHTML including 
+    //all styles
+    if (CWUtil.isObject(html) === true) {
+      var el = $(html);
+      var clone = el.clone();
+      clone[0].style.cssText = el[0].style.cssText; //TODO really needed?
+      html = clone[0].outerHTML;
+    }
+
+    //identifier can also be a CWDevice
+    if (CWDevice.prototype.isPrototypeOf(identifier)) {
+      identifier = identifier.getIdentifier();
+    }
+
+    var message = {
+      selector    : target,
+      html        : html,
+      contentOnly : contentOnly,
+    };
+    this.send(identifier, "_replace", message);
   },
 
 
@@ -1467,6 +1576,11 @@ var Connichiwa = OOP.createSingleton("Connichiwa", "Connichiwa", {
     if (callback !== undefined) {
       this.on("__messageack__id" + messageID, callback);
     }
+  },
+
+  "public loadCSS": function(identifier, url) {
+    var message = { url  : url };
+    var messageID = this.send(identifier, "_loadcss", message);
   },
 
 
@@ -1510,7 +1624,13 @@ var Connichiwa = OOP.createSingleton("Connichiwa", "Connichiwa", {
 
   "package _sendObject": function(message)
   {
+    if (("_name" in message) === false) {
+      console.warn("Tried to send message without _name, ignoring: "+JSON.stringify(message));
+      return;
+    }
+
     message._id = CWUtil.randomInt();
+    message._name = message._name.toLowerCase();
 
     var messageString = JSON.stringify(message);
     CWDebug.log(4, "Sending message: " + messageString);
@@ -2275,21 +2395,50 @@ OOP.extendSingleton("Connichiwa", "CWWebsocketMessageParser",
     device.connectionState = CWDeviceConnectionState.CONNECTED;
     nativeCallRemoteDidConnect(device.getIdentifier());
     
-    var connectedCallback = function() { 
+    var didConnectCallback = function() { 
       CWEventManager.trigger("deviceConnected", device); 
     };
+
+    var loadOtherFile = function(device, file) {
+      //As of now, "other" files are only CSS
+      var extension = file.split(".").pop().toLowerCase();
+      if (extension === "css") {
+        device.loadCSS(file);
+      } 
+    };
     
-    if (Connichiwa.autoLoadScripts.length > 0) {
-      for (var i = 0; i < Connichiwa.autoLoadScripts.length ; i++) {
-        var script = Connichiwa.autoLoadScripts[i];
-        if (i === (Connichiwa.autoLoadScripts.length - 1)) {
-          device.loadScript(script, connectedCallback);
+    //We need to separate JS files from other filetypes in Connichiwa.autoLoad
+    //The reason is that we want to attach a callback to the last JS file we
+    //load, so we are informed when it was loaded. 
+    var autoLoadJS    = [];
+    var autoLoadOther = [];
+    for (var i=0; i<Connichiwa.autoLoad.length; i++) {
+      var file = Connichiwa.autoLoad[i];
+      var extension = file.split(".").pop().toLowerCase();
+
+      if (extension === "js") autoLoadJS.push(file);
+      else autoLoadOther.push(file);
+    }
+
+    //First, let's load all non-JS files
+    for (var i=0; i<autoLoadOther.length; i++) {
+      var file = autoLoadOther[i];
+      loadOtherFile(device, file);
+    }
+
+    //Now load all JS files and attach the callback to the last one
+    //If no JS files are auto-loaded, execute the callback immediately
+    if (autoLoadJS.lenth > 0) {
+      for (var i=0; i<autoLoadJS.length; i++) {
+        var script = autoLoadJS[i];
+        if (i === (autoLoadJS.length - 1)) {
+          device.loadScript(script, didConnectCallback);
         } else {
           device.loadScript(script);
         }
       }
     } else {
-      connectedCallback();
+      didConnectCallback();
     }
   },
 
@@ -2312,7 +2461,7 @@ OOP.extendSingleton("Connichiwa", "CWWebsocketMessageParser",
 OOP.extendSingleton("Connichiwa", "Connichiwa", {
   "private _connectionAttempts" : 0,
   "public autoConnect": false,
-  "public autoLoadScripts": [],
+  "public autoLoad": [],
 
 
   // PUBLIC API
@@ -2329,6 +2478,21 @@ OOP.extendSingleton("Connichiwa", "Connichiwa", {
 
   "public isMaster": function() {
     return true;
+  },
+
+
+  "public getIPs": function() {
+    var localDevice = CWDeviceManager.getLocalDevice();
+    if (localDevice === undefined) return undefined;
+
+    return localDevice.getIPs();
+  },
+
+  "public getPort": function() {
+    var localDevice = CWDeviceManager.getLocalDevice();
+    if (localDevice === undefined) return undefined;
+
+    return localDevice.getPort();
   },
 
 
